3b71184ebe23bcea7d0a7dc49c11af4d5e0fdc0a
BRÜLISAUER Patricia <p.bruelisauer@eugster.ch>
2020-02-21 10:59:05 +0100

    step 3 - components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+   <h1>
+    Welcome to {{ title }}!
+  </h1>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'Meowdy! =^o^=';
 }
