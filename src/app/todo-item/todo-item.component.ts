2a4cb5c877948b8a017a36df815e2cf6ea8a95e1
BRÜLISAUER Patricia <p.bruelisauer@eugster.ch>
2020-02-21 16:20:01 +0100

    step 19 - adding checkbox

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
 
 
@@ -7,16 +7,22 @@ import { TodoItem } from '../interfaces/todo-item';
   template: `
   <div class="todo-item">
     {{ item.title }}
+    <button class="btn" (click)="removeItem()">Remove</button>
   </div>
+
+
   `,
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit  {
   @Input() item: TodoItem;
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  removeItem() {
+    this.remove.emit(this.item);
+  }
 }
